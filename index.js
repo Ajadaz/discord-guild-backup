e0b6d987d9e4046a60a9de395e84baf58f171006
Ajadaz <aidas.tekorius@gmail.com>
2023-10-15 13:13:09 +0300

    Add asset fetching, fix message fetching bug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import fetch from "node-fetch";
 import fs from "fs/promises";
-import { fetchWithDiscordAuth } from "./util.js";
+import { downloadFile, fetchWithDiscordAuth } from "./util.js";
 
 const DISCORD_API_ENDPOINT = "https://discord.com/api/v10";
+const prettyPrint = false;
 
 if(!process.argv[2])
 {
@@ -24,7 +24,7 @@ guild.channels = channels;
 
 console.log(`Backuping guild ${guild.name}...`);
 await fs.mkdir(guild.name, { recursive: true });
-await fs.writeFile(`${guild.name}/guild.json`, JSON.stringify(guild, null, 4));
+await fs.writeFile(`${guild.name}/guild.json`, JSON.stringify(guild, null, prettyPrint ? 4 : null));
 
 for(const channel of channels)
 {
@@ -34,10 +34,16 @@ for(const channel of channels)
     let afterId = "";
     do
     {
-        const queryString = afterId ? `?after=${afterId}` : "";
+        const queryString = afterId ? `?before=${afterId}` : "";
         const messageUrl = `${DISCORD_API_ENDPOINT}/channels/${channel.id}/messages${queryString}`;
         const messagesStepResp = await fetchWithDiscordAuth(messageUrl);
         messageStep = await messagesStepResp.json();
+        if(messagesStepResp.status === 429)
+        {
+            console.log(`Rate limit. Retrying in ${messageStep.retry_after}`);
+            await new Promise(resolve => setTimeout(resolve, messageStep.retry_after * 1000));
+            continue;
+        }
         afterId = messageStep[messageStep.length - 1]?.id;
         messages = messages.concat(messageStep);
     } while(messageStep.length > 0)
@@ -51,5 +57,77 @@ for(const channel of channels)
     console.log(`Found ${messages.length} messages. Saving to file...`);
 
     await fs.mkdir(`${guild.name}/${channel.name}`, { recursive: true });
-    await fs.writeFile(`${guild.name}/${channel.name}/messages.json`, JSON.stringify(messages, null, 4));
+
+    for(const [i, message] of messages.entries())
+    {
+        if(i % 50 === 0)
+        {
+            console.log(`${i}/${messages.length} Saving assets from messages...`)
+        }
+        for(const embed of message.embeds)
+        {
+            if(embed.thumbnail)
+            {
+                await getDiscordAsset(
+                    embed.thumbnail,
+                    "url",
+                    `${guild.name}/${channel.name}/thumb_${message.id}`,
+                    "proxy_url",
+                    `${guild.name}/${channel.name}/pthumb_${message.id}`);
+            }
+            if(embed.video && embed.video.url)
+            {
+                await getDiscordAsset(
+                    embed.video,
+                    "url",
+                    `${guild.name}/${channel.name}/vid_${message.id}`,
+                    "proxy_url",
+                    `${guild.name}/${channel.name}/pvid_${message.id}`);
+            }
+            if(embed.image)
+            {
+                await getDiscordAsset(
+                    embed.image,
+                    "url",
+                    `${guild.name}/${channel.name}/img_${message.id}`,
+                    "proxy_url",
+                    `${guild.name}/${channel.name}/pimg_${message.id}`);
+            }
+            if(embed.author && embed.author.icon_url)
+            {
+                await getDiscordAsset(
+                    embed.author,
+                    "icon_url",
+                    `${guild.name}/${channel.name}/auth_${message.id}`,
+                    "proxy_icon_url",
+                    `${guild.name}/${channel.name}/pauth_${message.id}`);
+            }
+        }
+
+        for(const attachment of message.attachments)
+        {
+            await getDiscordAsset(
+                attachment,
+                "url",
+                `${guild.name}/${channel.name}/${attachment.filename}`,
+                "proxy_url",
+                `${guild.name}/${channel.name}/p${attachment.filename}`);
+        }
+    }
+
+    await fs.writeFile(`${guild.name}/${channel.name}/messages.json`, JSON.stringify(messages, null, prettyPrint ? 4 : null));
+}
+
+async function getDiscordAsset(obj, targetProperty, fileName, proxyProperty, proxyFileName)
+{
+    const backupFile = await downloadFile(obj[targetProperty], fileName);
+    if(backupFile !== null)
+        obj.backupFile = backupFile;
+    else if(obj[proxyProperty])
+    {
+        console.log("Trying proxy...")
+        const backupFile = await downloadFile(obj[proxyProperty], proxyFileName);
+        if(backupFile !== null)
+            obj.proxyBackupFile = backupFile;
+    }
 }
--- util.js
+++ util.js
@@ -1,3 +1,6 @@
+import fetch from "node-fetch";
+import fs from "fs/promises";
+
 /** @param { URL | import("node-fetch").RequestInfo } url
  *  @param { import("node-fetch").RequestInit } [init]
  *  @return { Promise<Response> }
@@ -10,4 +13,62 @@ export function fetchWithDiscordAuth(url, init = {})
             "Authorization": `Bot ${process.env.AUTH_TOKEN}`
         }
     })
+}
+
+export async function downloadFile(url, fileName)
+{
+    let newFileName = await getUniqueFileName(fileName);
+    try
+    {
+        const request = await fetch(url);
+        const cont = await request.arrayBuffer();
+        if(request.status < 200 || request.status > 299)
+        {
+            console.log(`WARN Non 200 response on "${newFileName}" from "${url}". Dead file?`);
+            return null;
+        }
+        if(cont.byteLength === 0)
+        {
+            console.log(`WARN Empty file "${newFileName}" from "${url}". Dead file?`);
+            return null;
+        }
+        await fs.writeFile(newFileName, Buffer.from(cont));
+    }
+    catch (e)
+    {
+        console.log(`WARN Failed to write file "${newFileName}" from "${url}". Dead domain?`);
+        return null;
+    }
+    return newFileName;
+}
+
+async function getUniqueFileName(fileName)
+{
+    let newFileName = fileName;
+    let counter = null;
+    while(await fileExists(newFileName))
+    {
+        if(counter === null)
+            counter = 0;
+        const splitFileName = fileName.split(".")
+        if(splitFileName.length === 1)
+        {
+            newFileName = `${splitFileName[0]}_${counter}`;
+        }
+        else
+        {
+            splitFileName[splitFileName.length - 2] = `${splitFileName[splitFileName.length - 2]}_${counter}`;
+            newFileName = splitFileName.join(".");
+        }
+        counter++;
+    }
+    return newFileName;
+}
+
+// https://stackoverflow.com/questions/17699599/node-js-check-if-file-exists
+function fileExists(file)
+{
+    return fs.access(file, fs.constants.F_OK)
+        .then(() => true)
+        .catch(() => false)
 }

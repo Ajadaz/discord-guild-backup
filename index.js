b06de7804f2aa7b98d95d61d4d964f15d57772ee
Ajadaz <aidas.tekorius@gmail.com>
2023-10-15 10:14:52 +0300

    Get message fetching up and running

--- index.js
+++ index.js
@@ -16,8 +16,40 @@ if(!process.env.AUTH_TOKEN)
     process.exit(1);
 }
 
-const guild = await fetchWithDiscordAuth(`${DISCORD_API_ENDPOINT}/guilds/${process.argv[2]}`);
+const guildResp = await fetchWithDiscordAuth(`${DISCORD_API_ENDPOINT}/guilds/${process.argv[2]}`);
+const channelsResp = await fetchWithDiscordAuth(`${DISCORD_API_ENDPOINT}/guilds/${process.argv[2]}/channels`);
+let guild = await guildResp.json();
+let channels = await channelsResp.json();
+guild.channels = channels;
 
-const channels = await fetchWithDiscordAuth(`${DISCORD_API_ENDPOINT}/guilds/${process.argv[2]}/channels`);
+console.log(`Backuping guild ${guild.name}...`);
+await fs.mkdir(guild.name, { recursive: true });
+await fs.writeFile(`${guild.name}/guild.json`, JSON.stringify(guild, null, 4));
 
-console.log(await channels.json());
+for(const channel of channels)
+{
+    console.log(`Found channel ${channel?.name}`);
+    let messages = [];
+    let messageStep = [];
+    let afterId = "";
+    do
+    {
+        const queryString = afterId ? `?after=${afterId}` : "";
+        const messageUrl = `${DISCORD_API_ENDPOINT}/channels/${channel.id}/messages${queryString}`;
+        const messagesStepResp = await fetchWithDiscordAuth(messageUrl);
+        messageStep = await messagesStepResp.json();
+        afterId = messageStep[messageStep.length - 1]?.id;
+        messages = messages.concat(messageStep);
+    } while(messageStep.length > 0)
+
+    if(messages.length === 0)
+    {
+        console.log("No messages found. Skipping")
+        continue;
+    }
+
+    console.log(`Found ${messages.length} messages. Saving to file...`);
+
+    await fs.mkdir(`${guild.name}/${channel.name}`, { recursive: true });
+    await fs.writeFile(`${guild.name}/${channel.name}/messages.json`, JSON.stringify(messages, null, 4));
+}
